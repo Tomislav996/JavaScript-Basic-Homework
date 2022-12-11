78dc769a65011af6c845f332c2156da2f628b4d0
Tomislav996 <trpeskitomislav@outlook.com>
2022-12-11 19:28:52 +0100

    .

---
+++ HomeWork#3/JavaScript-files/Exercise2.js
@@ -0,0 +1,16 @@
+let Astring = "Text";
+let Anumber = 20;
+let Aboolean = true;
+let Aobject = Object;
+let Aundefined = undefined;
+
+
+function CheckType(value) {
+    console.log(typeof(value));
+}
+
+CheckType(Astring);
+CheckType(Anumber);
+CheckType(Aboolean);
+CheckType(Aobject);
+CheckType(Aundefined);
---
+++ HomeWork#3/JavaScript-files/Exercise3.js
@@ -0,0 +1,25 @@
+let User1stChoice = prompt("Enter 'Dog' to convert Human years to Dog years, or Enter 'Human' to convert Dog years to Human years: ")
+let User2stChoice = parseInt(prompt("Enter Years: "));
+
+function Convert(User1stChoice, User2stChoice) {
+  if (User1stChoice === "Dog" || User1stChoice === "dog") {
+    let result = User2stChoice * 7;
+    alert(`The dog is ${result} years old`);
+  }
+  else if (User1stChoice === "Human" || User1stChoice === "human") {
+    let result = User2stChoice / 7;
+    result = result.toFixed(0);
+    alert(`The human is ${result} years old`);
+  }
+  else if (User1stChoice != String) {
+    alert("Invalid input");
+  }
+  else if (User2stChoice != Number) {
+    alert("Invalid input");
+  }
+  else {
+    alert("invalid input");
+  }
+}
+
+Convert(User1stChoice, User2stChoice);
