cf77a27df2ddfd41d0bc784ede36a1583d1c3780
Tomislav996 <trpeskitomislav@outlook.com>
2022-12-11 16:20:53 +0100

    .

--- HomeWork#3/JavScript-files/Exercise#1.js
+++ HomeWork#3/JavScript-files/Exercise#1.js
@@ -1,1 +1,43 @@
-let year = parseInt(prompt("Enter your year of birth:"));
+let year = parseInt(prompt("Enter your year of birth: "));
+
+switch((year - 4) % 12) {
+    case 0:
+      alert("Your sign is: Rat");
+      break;
+    case 1:
+      alert("Your sign is: Ox");
+      break;
+    case 2:
+      alert("Your sign is: Tiger");
+      break;
+    case 3:
+      alert("Your sign is: Rabbit");
+    case 4:
+      alert("Your sign is: Dragon");
+      break;
+    case 5:
+      alert("Your sign is: Snake");
+      break;
+    case 6:
+       alert("Your sign is: Horse");
+       break;
+    case 7:
+       alert("Your sign is: Goat");
+       break;
+    case 8:
+        alert("Your sign is: Monkey");
+        break;
+    case 9:
+        alert("Your sign is: Rooster");
+        break;
+    case 10:
+        alert("Your sign is: Dog");
+        break;
+    case 11:
+        alert("Your sign is: Pig");
+        break;
+    default:
+        alert("Invalid input!");
+        break;
+}
+
