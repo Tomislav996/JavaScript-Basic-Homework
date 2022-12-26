1ec88f3e53b51f6603b686a7593316938e0f90b6
Tomislav996 <trpeskitomislav@outlook.com>
2022-12-26 16:13:39 +0100

    .

---
+++ HomeWork#8/JavaScript/exercise1.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    let input = $("#input");
+    let button = $("#button");
+    let result = $("#result");
+    let clearBtn = $(`#clear`);
+    
+    button.click(function(){
+        let Name = input[0].value
+        result.text(`Hello There ${Name}`).css("color", "blue");
+        
+    })
+     clearBtn.click(function(){
+        result.text(``);
+
+    })
+});
---
+++ HomeWork#8/JavaScript/exercise2.js
@@ -0,0 +1,262 @@
+
+
+
+
+$(document).ready(function(){
+    let CSS_COLORS = [
+        "AliceBlue",
+        "AntiqueWhite",
+        "Aqua",
+        "Aquamarine",
+        "Azure",
+        "Beige",
+        "Bisque",
+        "Black",
+        "BlanchedAlmond",
+        "Blue",
+        "BlueViolet",
+        "Brown",
+        "BurlyWood",
+        "CadetBlue",
+        "Chartreuse",
+        "Chocolate",
+        "Coral",
+        "CornflowerBlue",
+        "Cornsilk",
+        "Crimson",
+        "Cyan",
+        "DarkBlue",
+        "DarkCyan",
+        "DarkGoldenRod",
+        "DarkGray",
+        "DarkGrey",
+        "DarkGreen",
+        "DarkKhaki",
+        "DarkMagenta",
+        "DarkOliveGreen",
+        "DarkOrange",
+        "DarkOrchid",
+        "DarkRed",
+        "DarkSalmon",
+        "DarkSeaGreen",
+        "DarkSlateBlue",
+        "DarkSlateGray",
+        "DarkSlateGrey",
+        "DarkTurquoise",
+        "DarkViolet",
+        "DeepPink",
+        "DeepSkyBlue",
+        "DimGray",
+        "DimGrey",
+        "DodgerBlue",
+        "FireBrick",
+        "FloralWhite",
+        "ForestGreen",
+        "Fuchsia",
+        "Gainsboro",
+        "GhostWhite",
+        "Gold",
+        "GoldenRod",
+        "Gray",
+        "Grey",
+        "Green",
+        "GreenYellow",
+        "HoneyDew",
+        "HotPink",
+        "IndianRed",
+        "Indigo",
+        "Ivory",
+        "Khaki",
+        "Lavender",
+        "LavenderBlush",
+        "LawnGreen",
+        "LemonChiffon",
+        "LightBlue",
+        "LightCoral",
+        "LightCyan",
+        "LightGoldenRodYellow",
+        "LightGray",
+        "LightGrey",
+        "LightGreen",
+        "LightPink",
+        "LightSalmon",
+        "LightSeaGreen",
+        "LightSkyBlue",
+        "LightSlateGray",
+        "LightSlateGrey",
+        "LightSteelBlue",
+        "LightYellow",
+        "Lime",
+        "LimeGreen",
+        "Linen",
+        "Magenta",
+        "Maroon",
+        "MediumAquaMarine",
+        "MediumBlue",
+        "MediumOrchid",
+        "MediumPurple",
+        "MediumSeaGreen",
+        "MediumSlateBlue",
+        "MediumSpringGreen",
+        "MediumTurquoise",
+        "MediumVioletRed",
+        "MidnightBlue",
+        "MintCream",
+        "MistyRose",
+        "Moccasin",
+        "NavajoWhite",
+        "Navy",
+        "OldLace",
+        "Olive",
+        "OliveDrab",
+        "Orange",
+        "OrangeRed",
+        "Orchid",
+        "PaleGoldenRod",
+        "PaleGreen",
+        "PaleTurquoise",
+        "PaleVioletRed",
+        "PapayaWhip",
+        "PeachPuff",
+        "Peru",
+        "Pink",
+        "Plum",
+        "PowderBlue",
+        "Purple",
+        "RebeccaPurple",
+        "Red",
+        "RosyBrown",
+        "RoyalBlue",
+        "SaddleBrown",
+        "Salmon",
+        "SandyBrown",
+        "SeaGreen",
+        "SeaShell",
+        "Sienna",
+        "Silver",
+        "SkyBlue",
+        "SlateBlue",
+        "SlateGray",
+        "SlateGrey",
+        "Snow",
+        "SpringGreen",
+        "SteelBlue",
+        "Tan",
+        "Teal",
+        "Thistle",
+        "Tomato",
+        "Turquoise",
+        "Violet",
+        "Wheat",
+        "White",
+        "WhiteSmoke",
+        "Yellow",
+        "YellowGreen",
+      ];
+    let Input1 = $(`#textInput`);
+
+    let Input2 = $(`#colorInput`);
+
+    let Button = $(`#button`);
+
+
+    let error = $(`#message`);
+
+    Button.click(function(){
+        let text = Input1[0].value;
+        let color = Input2[0].value;
+        if(text !== `` && CSS_COLORS.includes(`${color}`)) {
+            $(`<h1>${text}</h1>`).insertAfter("#Header").css("color",`${color}`);
+        }
+        else {
+            error.text(`Invalid input`).css("color", "green");
+        }
+        
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let studentForm = document.getElementById("studentForm");
+let resultDiv = document.getElementById("resultDiv");
+
+let students = [];
+
+function Student(first, last, age) {
+    this.firstName = first;
+    this.lastName = last;
+    this.studentsAge = age;
+}
+
+function showStudents(studentsArr, element) {
+    element.innerHTML = '';
+    for (let student of studentsArr) {
+        element.innerHTML += `<li><b>Name:</b> ${student.firstName} ${student.lastName} <b>Age:</b> ${student.studentsAge}</li>`
+    }
+}
+
+let submitBtn = document.getElementsByTagName("button")[0];
+
+submitBtn.addEventListener('click', function() {
+    let firstName = studentForm.children[0].value;
+    let lastName = studentForm.children[1].value;
+    let age = studentForm.children[2].value;
+
+    let newStudent = new Student(firstName, lastName, age);
+    
+    students.push(newStudent);
+
+    let list = document.getElementsByTagName('ul')[0];
+
+    showStudents(students, list);
+
+    studentForm.children[0].value = '';
+    studentForm.children[1].value = '';
+    studentForm.children[2].value = '';
+
+})
+*/
