3898c0236bdd63b4fc9dad938ec8767311f763d7
Tomislav996 <trpeskitomislav@outlook.com>
2022-12-30 17:23:30 +0100

    Update

--- HomeWork#9/JavaScript/exercise1.js
+++ HomeWork#9/JavaScript/exercise1.js
@@ -1,30 +1,85 @@
 
 $(document).ready(function(){
-    let button = $("#button")
-    button.click(function(){
-      $.ajax({
-        method: "GET",
-        url: `https://jsonplaceholder.typicode.com/todos/`,
-        success: function (data) {
+  let button = $("#button")
+  button.click(function(){
+    $.ajax({
+      method: "GET",
+      url: `https://jsonplaceholder.typicode.com/todos/`,
+      success: function (data) {
+        if(Array.isArray(data)){     
           printAll(data);
-          
+        } 
+        else {
+          console.log("Error, this item is not an array.");
         }
-      })
-  
+        
+        
+      },
+      error: function(err) {
+        console.log("This is an error message", err);
+      }
     })
-  
-  
-    function printAll(data){
-      for(let i = 0; i < 200; i++)
-      if(data[i].completed) {
-        console.log(`The ID of this person is ${data[i].id} its Title is ${data[i].title} and its task is Completed`);
-      }
-      else{
-        console.log(`The ID of this person is ${data[i].id} its Title is ${data[i].title} and its task is not Completed`);
-      }
-      
-  
-    }
-  
+   
+
   })
-  
+
+  function printAll(data){
+    for(let i = 0; i < data.length; i++)
+    if(data[i].completed) {
+      console.log(`The ID of this person is ${data[i].id} its Title is ${data[i].title} and its task is Completed`);
+    }
+    else{
+      console.log(`The ID of this person is ${data[i].id} its Title is ${data[i].title} and its task is not Completed`);
+    }
+    
+
+  }
+
+})
+
+
+/*
+$(document).ready(function(){
+  let button = $("#button");
+  button.click(function(){
+    fetch(`https://jsonplaceholder.typicode.com/todos/`)
+    .then(response => {
+      response.json()
+        .then(data => {
+          if(Array.isArray(data)){
+            printAll(data);
+          }
+          else {
+            console.log("Error, this item is not an array.");
+          }
+           
+        })
+
+      
+    })
+
+    .catch(error => {
+      console.log("This is an error message", error);
+    })
+  })
+
+  function printAll(data){
+    for(let i = 0; i < data.length; i++)
+    if(data[i].completed) {
+      console.log(`The ID of this person is ${data[i].id} its Title is ${data[i].title} and its task is Completed`);
+    }
+    else{
+      console.log(`The ID of this person is ${data[i].id} its Title is ${data[i].title} and its task is not Completed`);
+    }
+    
+
+  }
+
+
+
+})
+
+*/
+
+
+
